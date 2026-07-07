c948ce31800e1f9294751dec2eb9b53cbb602b23
Nicole Tregoning <nicole.tregoning@jhuapl.edu>
2026-07-07 14:07:59 -0400

    add import statements in case needed

--- terraform/email_forwarding.tf
+++ terraform/email_forwarding.tf
@@ -61,8 +61,18 @@ resource "improvmx_domain" "sandbox" {
   domain   = "sandbox.emberarchive.org"
 }
 
+import {
+  to = improvmx_domain.sandbox
+  id = "sandbox.emberarchive.org"
+}
+
 resource "improvmx_email_forward" "sandbox_info" {
   domain            = improvmx_domain.sandbox.domain
   alias_name        = "info"
   destination_email = "bbqs-ember-admin@jhuapl.edu"
+}
+
+import {
+  to = improvmx_email_forward.sandbox_info
+  id = "sandbox.emberarchive.org_info"
 }
